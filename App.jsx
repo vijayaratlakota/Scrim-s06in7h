d739c8294efd377388c3c7fafe753b2533491a22
ProfessorVij <133563290+vijayaratlakota@users.noreply.github.com>
2026-09-19 12:59:55 +0530

    update-3

--- App.jsx
+++ App.jsx
@@ -36,6 +36,7 @@ export default function App() {
     const entryElements = data.map((entry) => {
         return (
             <Entry
+                key={entry.id}
                 img={entry.img}
                 title={entry.title}
                 country={entry.country}
